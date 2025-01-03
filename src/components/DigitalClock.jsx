456c3967a5b27475ce0407b7d8afde0e9618df31
Kevin David Zambrano Galvis <kevin.zambrano@aerocivil.gov.co>
2025-01-03 11:23:51 -0500

    agregando el componente reloj digital

---
+++ src/components/DigitalClock.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DigitalClock = () =>{
+    return(
+        <h1>HOLA MUNDO REACT</h1>
+    )
+}
+
+export default DigitalClock;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import DigitalClock from './components/DigitalClock';
 
 
 
@@ -7,6 +8,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <DigitalClock />
   </React.StrictMode>
 );
