95cc88cece91868d60e8538d81aa93859c3b20db
Kevin David Zambrano Galvis <kevin.zambrano@aerocivil.gov.co>
2025-01-03 16:02:53 -0500

    creando componente reloj

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import '../css/navigation.css';
 
 const DigitalClock = () =>{
     return(
-        <h1>HOLA MUNDO REACT</h1>
+        <div className="container-navigation" >
+            <div className="digital-clock">
+                <span className="day-clock">01</span>
+                <span className="month-clock">Septiembre</span>
+                <span className="year-clock">2025</span>
+                <span>:</span>
+                <span className="hours-clock">10</span>
+                <span>:</span>
+                <span className="minute-clock">10</span>
+                <span>:</span>
+                <span className="secongs-clock">59</span>
+                <span className="AM-clock">AM</span>
+            </div>
+
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './css/main.css';
 import DigitalClock from './components/DigitalClock';
 
 
